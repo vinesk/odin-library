37082cc0af37d7d9ad494f97c8a6405ef2bb8423
vinesk <vinesk.dev@gmail.com>
2023-11-24 15:18:02 +0100

    Add remove buttons

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 // DOM selectors
 const form = document.querySelector(".form");
-const bookCards = document.querySelector(".book-cards");
-const addButton = document.querySelector(".btn-add");
 const plusButton = document.querySelector(".btn-plus");
 const minusButton = document.querySelector(".btn-minus");
-const readButtons = document.querySelectorAll(".btn-read");
-const removeButtons = document.querySelectorAll(".btn-remove");
+const bookCards = document.querySelector(".book-cards");
 
 // Books
 const books = [
@@ -23,11 +20,17 @@ const books = [
   },
 ];
 
+// Book constructor
+const Book = function (title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+};
+
 // Generate book cards
 const generateBookCards = (books) => {
-  bookCards.innerHTML = "";
-
-  for (const book of books) {
+  for (let i = 0; i < books.length; i++) {
     // Create elements
     const bookCard = document.createElement("article");
     const title = document.createElement("h2");
@@ -36,7 +39,7 @@ const generateBookCards = (books) => {
     const readButton = document.createElement("button");
     const removeButton = document.createElement("button");
 
-    // Add classes
+    // Add attributes
     bookCard.classList.add("book-card");
     title.classList.add("title");
     author.classList.add("author");
@@ -44,15 +47,17 @@ const generateBookCards = (books) => {
     readButton.classList.add(
       "btn",
       "btn-read",
-      book.isRead ? "btn-green" : "btn-red"
+      books[i].isRead ? "btn-green" : "btn-red"
     );
+    readButton.dataset.id = i;
     removeButton.classList.add("btn", "btn-remove");
+    removeButton.dataset.id = i;
 
     // Add text contents
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = `${book.pages} pages`;
-    readButton.textContent = book.isRead ? "Read" : "Not read";
+    title.textContent = books[i].title;
+    author.textContent = books[i].author;
+    pages.textContent = `${books[i].pages} pages`;
+    readButton.textContent = books[i].isRead ? "Read" : "Not read";
     removeButton.textContent = "Remove";
 
     // Create card
@@ -65,50 +70,67 @@ const generateBookCards = (books) => {
     // Add card to the DOM
     bookCards.appendChild(bookCard);
   }
+  readBook();
+  removeBook();
+};
+
+// Add book
+const addBook = () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const isRead = document.querySelector("#read").checked;
+    const book = new Book(title, author, pages, isRead);
+    books.push(book);
+    bookCards.innerHTML = "";
+    generateBookCards(books);
+  });
+};
+
+// Sort in ascending order
+const sortInAscendingOrder = () => {
+  plusButton.addEventListener("click", () => {
+    books.sort((a, b) => (a.title < b.title ? -1 : 1));
+    bookCards.innerHTML = "";
+    generateBookCards(books);
+  });
+};
+
+// Sort in descending order
+const sortInDescendingOrder = () => {
+  minusButton.addEventListener("click", () => {
+    books.sort((a, b) => (a.title > b.title ? -1 : 1));
+    bookCards.innerHTML = "";
+    generateBookCards(books);
+  });
+};
+
+// Read book
+const readBook = () => {
+  const readButtons = document.querySelectorAll(".btn-read");
+  for (const readButton of readButtons) {
+    readButton.addEventListener("click", (e) => {
+      console.log(e.target.dataset.id);
+    });
+  }
+};
+
+// Remove book
+const removeBook = () => {
+  const removeButtons = document.querySelectorAll(".btn-remove");
+  for (const removeButton of removeButtons) {
+    removeButton.addEventListener("click", (e) => {
+      books.splice(e.target.dataset.id, 1);
+      bookCards.innerHTML = "";
+      generateBookCards(books);
+    });
+  }
 };
 
 // First display
 generateBookCards(books);
-
-// Sort in ascending order
-plusButton.addEventListener("click", () => {
-  const newBooks = Array.from(books);
-  newBooks.sort((a, b) => (a.title < b.title ? -1 : 1));
-  generateBookCards(newBooks);
-});
-
-// Sort in descending order
-minusButton.addEventListener("click", () => {
-  const newBooks = Array.from(books);
-  newBooks.sort((a, b) => (a.title > b.title ? -1 : 1));
-  generateBookCards(newBooks);
-});
-
-// Book constructor
-const Book = function (title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-};
-
-// Add book to book list
-const addBookTobooks = (books, book) => {
-  books.push(book);
-};
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const newBooks = Array.from(books);
-
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const isRead = document.querySelector("#read").checked;
-
-  const book = new Book(title, author, pages, isRead);
-  addBookTobooks(newBooks, book);
-
-  generateBookCards(newBooks);
-});
+addBook();
+sortInAscendingOrder();
+sortInDescendingOrder();
